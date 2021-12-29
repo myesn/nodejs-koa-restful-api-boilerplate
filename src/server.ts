35de10290a7901cd416e625bc826d376f56940cf
myesn <myesn@foxmail.com>
2021-12-29 12:06:14 +0800

    :tada: 集成 koa + typescript，并新增大量 package.json 的 scripts

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import Koa from "koa";
+
+const server: Koa = new Koa();
+const port: number = 3000;
+
+server.use((ctx: Koa.DefaultContext) => {
+  ctx.body = "hi";
+});
+
+server.listen(port, () => {
+  console.log(`Node.js v${process.versions.node}`);
+  console.log(`Server running on http://localhost:${port}`);
+});
