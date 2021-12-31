4ec181224a16d338c49801a35f9af7e180596de4
myesn <myesn@foxmail.com>
2021-12-31 11:00:45 +0800

    :sparkles: 1. 引入 dotenv(preload 模式); 2. 增加本地(.env.development) 和部署(.env.production)配置文件; 3.与 node、nodemon 集成，实现 hot reload 功能; 4. 不同运行环境指定不同的配置文件(package.json > scripts); 4. 从 .gitignore 中注释 .env.* 文件的忽略项配置

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,7 @@ server.use((ctx: Koa.DefaultContext) => {
 
 server.listen(port, () => {
   console.log(`Node.js v${process.versions.node}`);
+  console.log(`Node.js environment: ${process.env.NODE_ENV}`);
+  console.log(`Node.js process.env: ${process.env.DB_HOST}`);
   console.log(`Server running on http://localhost:${port}`);
 });
